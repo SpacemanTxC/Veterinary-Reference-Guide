edebba0fe02a7e1595b500dfd6ac53eb88a27d29
Tate Capra <tatecapra@outlook.com>
2020-04-06 18:35:56 -0500

    added a main js with notes

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+// need to create a function for the Quiz
+  //four parts are needed for the quiz: questions, place for them, results to show, and button to use
+function quizGenerator(questions, quizContainer, resultsContainer, submitButton){
+
+      function displayQuiz(questions, quizContainer){
+          //code for the questions
+      }
+}
