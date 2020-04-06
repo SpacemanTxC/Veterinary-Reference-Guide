a0ec179c057de78ab98a486e72894ee865911745
Tate Capra <tatecapra@outlook.com>
2020-04-06 18:49:12 -0500

    started to write the code for the js, including the functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,19 @@ function quizGenerator(questions, quizContainer, resultsContainer, submitButton)
       function displayQuiz(questions, quizContainer){
           //code for the questions
       }
-}
+
+      function showResults(questions, quizContainer, resultsContainer){
+          // code for results
+      }
+
+      //call the function
+      displayQuiz(questions, quizContainer);
+
+      //creatng an onclick function for the button
+      submitButton.onclick = function(){
+          //button calls the show results function
+          showResults(questions, quizContainer, resultsContainer);
+      }
+
+// array will store the questions and correct answers
+var myQuestions = [];
