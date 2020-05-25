af2ac827ef24a3bf225acc4f6df9483a4ae8583b
Tate Capra <tatecapra@outlook.com>
2020-05-25 12:56:53 -0500

    Added syntax of project outline to app.js

--- vet-app/src/App.js
+++ vet-app/src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Vetenary Reference Guides</h1>
+        // Project Outline
+        /* Vet app will display vetenary information.
+            app will be made using Bootstrap as the libarey for class
+            app will have four major rows
+            1) Vet Ref Header with menu drop down on the left
+            2) Store images: onclick for img will display text in row 3 & 4
+            3)Title of diagnosis
+            4)Information about the clicked photo: Full Text Here
+              in row 4 reset button for text and title on bottom of text
+
+            App should display title, photo, and text- Storage for information
+            */
+
       </header>
     </div>
   );
