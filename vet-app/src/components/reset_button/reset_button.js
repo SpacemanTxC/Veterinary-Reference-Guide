1cd20aa0728f75e3225b89690a747633319567a8
Tate Capra <tatecapra@outlook.com>
2020-06-12 18:55:28 -0500

    Added components with files etc and added import to files

---
+++ vet-app/src/components/reset_button/reset_button.js
@@ -0,0 +1,3 @@
+import React from 'react';
+//import style
+import 'reset_button';
--- vet-app/src/index.js
+++ vet-app/src/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// import style
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import components
+import 'components/head_banner/head_banner.js'
+import 'components/photo_display/photo_display.js'
+import 'components/reset_button/reset_button.js'
+import 'components/text_display/text_display.js'
+import 'components/title_banner/title_banner.js'
+
+
 
 // JS File Here
 //Link all Components
 
-// Add Steps Comments 
+//App
+class App extends React.Component {
+
+  //What will be displayed
+  render(){
+    return (
+      <div className = "app">
+        <h1>Veterinary App</h1>
+      </div>
+    )
+  }
+}
+
+// Add Steps Comments
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-serviceWorker.unregister();
